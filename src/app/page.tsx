a236d6f055c949f1e2066a45003323d4cf205a5a
Max Shen <m4xshen@gmail.com>
2023-12-08 22:44:40 +0800

    feat: mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,83 @@
 'use client';
 
-import { Button } from '@nextui-org/react';
-import { useState } from 'react';
+import { Button, CircularProgress } from '@nextui-org/react';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [timer, setTimer] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
-  const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
+  const [totalTime, setTotalTime] = useState(0);
+  const [time, setTime] = useState(0);
+  const [mode, setMode] = useState('focus');
 
-  const startTimer = () => {
-    console.log('start');
-    setIntervalId(
-      setInterval(() => {
-        setTimer(prevTimer => prevTimer + 1);
-      }, 1000),
-    );
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (isRunning && mode === 'focus') {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else if (isRunning && mode === 'break') {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+
+    if (!isRunning && time !== 0 && mode === 'focus') {
+      setMode('break');
+      setTime(Math.floor(time / 5));
+      setTotalTime(Math.floor(time / 5));
+    }
+
+    if (time === 0 && mode === 'break') {
+      setMode('focus');
+      setIsRunning(false);
+      setTime(0);
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [isRunning, time, mode]);
+
+  const toggleTimer = () => {
+    setIsRunning(!isRunning);
   };
 
-  const stopTimer = () => {
-    console.log('stop');
-    if (intervalId) {
-      clearInterval(intervalId);
+  const formatTime = (t: number) => {
+    const minutes = Math.floor(t / 60);
+    const seconds = t % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  };
+
+  const buttonText = () => {
+    if (isRunning) {
+      return 'Stop';
     }
-    setIsRunning(false);
+    if (mode === 'focus') {
+      return 'Start Focus';
+    }
+    return 'Start Break';
   };
 
   return (
-    <>
-      <Button
-        onClick={() => {
-          if (isRunning) {
-            stopTimer();
-          } else {
-            startTimer();
-          }
-          setIsRunning(!isRunning);
+    <div className="flex flex-col items-center justify-center">
+      <CircularProgress
+        value={mode === 'focus' ? 100 : (100 * time) / totalTime}
+        size="lg"
+        showValueLabel
+        valueLabel={formatTime(time)}
+        classNames={{
+          svg: 'w-52 h-52 text-[#41D1FF]',
+          value: 'text-3xl font-semibold',
         }}
-      >
-        {isRunning ? 'Stop' : 'Start'}
+      />
+      <Button variant="flat" onClick={toggleTimer}>
+        {buttonText()}
       </Button>
-      {timer}
-    </>
+    </div>
   );
 }
