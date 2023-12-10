f8edc4b7a232c3be6f0d144f1da2de5cdeb6ac80
Max Shen <m4xshen@gmail.com>
2023-12-10 10:34:15 +0800

    feat: hide time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,15 @@
 'use client';
 
+import { Hide, Play, Show, Stop } from '@/components/Icons';
 import { Button, CircularProgress } from '@nextui-org/react';
 import { useState, useEffect } from 'react';
-
-function PlayIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="white"
-      height="16"
-      width="12"
-      viewBox="0 0 384 512"
-    >
-      <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
-    </svg>
-  );
-}
-
-function StopIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="white"
-      height="16"
-      width="12"
-      viewBox="0 0 384 512"
-    >
-      <path d="M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
-    </svg>
-  );
-}
 
 export default function Home() {
   const [isRunning, setIsRunning] = useState(false);
   const [totalTime, setTotalTime] = useState(0);
   const [time, setTime] = useState(0);
   const [mode, setMode] = useState('focus');
+  const [showTime, setShowTime] = useState(true);
 
   useEffect(() => {
     let interval: NodeJS.Timeout | null = null;
@@ -77,6 +51,8 @@ export default function Home() {
   };
 
   const formatTime = (t: number) => {
+    if (!showTime) return '**:**';
+
     const minutes = Math.floor(t / 60);
     const seconds = t % 60;
     return `${minutes.toString().padStart(2, '0')}:${seconds
@@ -92,6 +68,14 @@ export default function Home() {
         showValueLabel
         valueLabel={
           <div className="flex flex-col items-center gap-2">
+            <Button
+              size="sm"
+              variant="flat"
+              isIconOnly
+              onClick={() => setShowTime(!showTime)}
+            >
+              {showTime ? <Hide /> : <Show />}
+            </Button>
             {formatTime(time)}
             <span className="text-2xl">
               {mode === 'focus' ? 'Focus' : 'Break'}
@@ -104,7 +88,7 @@ export default function Home() {
         }}
       />
       <Button variant="solid" onClick={toggleTimer} isIconOnly>
-        {isRunning ? <StopIcon /> : <PlayIcon />}
+        {isRunning ? <Stop /> : <Play />}
       </Button>
     </div>
   );
