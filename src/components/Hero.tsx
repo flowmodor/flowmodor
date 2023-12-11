dd45b7996b740aebc0c583d0932af24c67ccf61f
Max Shen <m4xshen@gmail.com>
2023-12-11 15:09:24 +0800

    feat: refine hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,11 @@
 export default function Hero() {
   return (
-    <div className="mt-20 flex flex-col items-center">
+    <div className="mt-20 text-center w-full flex flex-col items-center">
       <div className="font-bold text-6xl">Flow and Focus</div>
       <div className="font-bold text-6xl text-[#D6B6FF]">Beyond Pomodoro</div>
-      <div className="pt-10 text-[#dddddd]">
-        Flowmodor is a flowmodoro timer web app based on the Flowtime Technique.
+      <div className="pt-10 w-4/5 max-w-xl text-[#dddddd]">
+        Flowmodor is an open-source flowmodoro timer web app based on the
+        Flowtime Technique, an alternative to the Pomodoro Technique.
       </div>
     </div>
   );
