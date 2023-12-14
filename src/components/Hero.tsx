a442c7070a62a0537cdf2534d264bc09eae7d040
Max Shen <m4xshen@gmail.com>
2023-12-14 20:41:06 +0800

    feat: add landing page features section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,20 @@
 export default function Hero() {
   return (
-    <div className="mt-20 text-center w-full flex flex-col items-center">
-      <div className="font-bold text-6xl">Flow and Focus</div>
-      <div className="font-bold text-6xl text-[#D6B6FF]">Beyond Pomodoro</div>
-      <div className="pt-10 w-4/5 max-w-xl text-[#dddddd]">
+    <div className="mt-20 text-center w-full flex gap-10 flex-col items-center">
+      <div>
+        <div className="font-bold text-6xl">Flow and Focus</div>
+        <div className="font-bold text-6xl text-[#D6B6FF]">Beyond Pomodoro</div>
+      </div>
+      <div className="w-4/5 max-w-xl text-[#dddddd]">
         Flowmodor is an open-source flowmodoro timer web app based on the
         Flowtime Technique, an alternative to the Pomodoro Technique.
       </div>
+      <a
+        href="#getWaitlistContainer"
+        className="font-semibold cursor-pointer rounded-md p-3 text-[#23223C] bg-[#D6B6FF]"
+      >
+        Get Started
+      </a>
     </div>
   );
 }
