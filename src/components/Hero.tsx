75b7363ccbdac137f9396c3a36c640b84c052a9c
Max Shen <m4xshen@gmail.com>
2023-12-15 22:53:00 +0800

    feat: refine landing page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,31 +1,37 @@
 export default function Hero() {
   return (
     <div
-      className="pt-24 text-center w-full h-screen bg-[#DBBFFF] flex gap-10
-        flex-col items-center text-[#23223C]"
+      className="flex flex-col xl:flex-row w-full h-screen bg-[#DBBFFF]
+        items-center justify-center gap-10 xl:gap-0"
     >
-      <div>
-        <div className="font-bold text-6xl">Flow and Focus</div>
-        <div className="font-bold text-6xl">Beyond Pomodoro</div>
+      <div className="flex gap-10 flex-col items-center xl:items-start text-[#23223C]">
+        <div className="text-center xl:text-start font-bold text-6xl">
+          <div>Flow and Focus</div>
+          <div>Beyond Pomodoro</div>
+        </div>
+        <div className="w-4/5 max-w-xl text-center xl:text-start">
+          Flowmodor is an open-source flowmodoro timer web app based on the
+          Flowtime Technique, an alternative to the Pomodoro Technique.
+        </div>
+        <div className="flex gap-5">
+          <a
+            href="#getWaitlistContainer"
+            className="font-semibold cursor-pointer rounded-md p-3 text-white bg-[#23223C]"
+          >
+            Get Started
+          </a>
+          <a
+            href="https://github.com/flowmodor/flowmodor"
+            className="font-semibold cursor-pointer rounded-md p-3
+              border-2 border-[#23223C] text-[#23223C]"
+          >
+            Star on GitHub
+          </a>
+        </div>
       </div>
-      <div className="w-4/5 max-w-xl">
-        Flowmodor is an open-source flowmodoro timer web app based on the
-        Flowtime Technique, an alternative to the Pomodoro Technique.
-      </div>
-      <a
-        href="#getWaitlistContainer"
-        className="font-semibold cursor-pointer rounded-md p-3 text-white bg-[#23223C]"
-      >
-        Get Started
-      </a>
       <img
-        src="/phone.png"
-        className="absolute bottom-0 translate-y-44 w-48 sm:w-64 left-[20%] lg:left-1/4 z-10"
-      />
-      <img
-        src="/laptop.png"
-        width={600}
-        className="hidden md:block absolute bottom-0 translate-y-44 right-0 lg:right-[10%]"
+        src="/mock.png"
+        className="hidden xl:block w-[40rem] flex-shrink-0"
       />
     </div>
   );
