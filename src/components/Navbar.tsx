a203ee49589393cb71e8470b65260e0ed4bd6c75
Max Shen <m4xshen@gmail.com>
2023-12-16 11:21:54 +0800

    feat: navbar dynamic background

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+
+export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight - 1;
+      if (window.scrollY > windowHeight) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav
+      className={`sticky top-0 z-10 justify-center backdrop-blur border-[#FFFFFF20] border-b flex -mb-16 h-16 items-center transition-all duration-500 ${
+        isScrolled ? "text-white" : "text-[#23223C]"
+      }`}
+    >
+      <a
+        href="/"
+        className="left-5 absolute flex cursor-pointer items-center gap-2 text-2xl font-bold"
+      >
+        <img src="/logo.png" width={32} className="rounded-md" />
+        <div>Flowmodor</div>
+      </a>
+      <div className="hidden sm:flex gap-10 font-semibold text-lg">
+        <a href="/" className="cursor-pointer">
+          Blog
+        </a>
+        <a href="/" className="cursor-pointer">
+          Pricing
+        </a>
+      </div>
+      <a
+        href="#getWaitlistContainer"
+        className={`right-5 absolute font-medium cursor-pointer rounded-md p-2 transition-all ${
+          isScrolled ? "text-[#23223C] bg-[#DBBFFF]" : "text-white bg-[#23223C]"
+        }`}
+      >
+        Get Started
+      </a>
+    </nav>
+  );
+}
