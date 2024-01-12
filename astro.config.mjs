743e419513f68bf57922db20ffb8fa7a4a5f6ca5
Max Shen <m4xshen@gmail.com>
2024-01-12 15:34:16 +0800

    chore: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  // Enable React to support React JSX components.
+  site: 'https://flowmodor.com',
   integrations: [react(), tailwind()]
 });
