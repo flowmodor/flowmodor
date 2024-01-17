acd5afbf936894c31bf00423beab8b182dda9629
Max Shen <m4xshen@gmail.com>
2024-01-17 13:23:13 +0800

    chore: remove unused package

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createBrowserClient(
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 );
