29e43eb5e126addc30602b7cf62ec0687bbd2e26
Max Shen <m4xshen@gmail.com>
2024-01-21 13:59:26 +0800

    fix: build error

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -5,7 +5,8 @@ import { Button } from '@nextui-org/button';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  // process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY,
 );
 
 export default function Invite() {
