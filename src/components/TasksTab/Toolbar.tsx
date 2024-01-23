da72ba069695e6c322cee549af6f8824f3929d4c
Max Shen <m4xshen@gmail.com>
2024-01-23 10:45:13 +0800

    feat: refine task toolbar

--- src/components/TasksTab/Toolbar.tsx
+++ src/components/TasksTab/Toolbar.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 import { Button } from '@nextui-org/button';
 import { Plus } from '@/components/Icons';
 import supabase from '@/utils/supabase';
-import { toast } from 'react-toastify';
+import { Input } from '@nextui-org/input';
 
 export default function Toolbar() {
   const [inputValue, setInputValue] = useState<string>('');
+  const isDisabled = inputValue.trim() === '';
 
   const onAddTask = async (name: string | undefined) => {
     if (!name) {
@@ -21,13 +23,18 @@ export default function Toolbar() {
 
   return (
     <>
-      <input
+      <Input
         className="w-full bg-transparent pr-5 text-[16px] outline-none"
         placeholder="Enter task name"
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        classNames={{
+          inputWrapper:
+            'h-full bg-transparent data-[hover=true]:bg-transparent data-[focus=true]:!bg-transparent',
+          input: 'text-[16px]',
+        }}
+        onValueChange={setInputValue}
         onKeyDown={(e) => {
-          if (inputValue.trim() === '') {
+          if (isDisabled) {
             return;
           }
 
@@ -41,18 +48,12 @@ export default function Toolbar() {
         type="button"
         variant="flat"
         isIconOnly
-        onClick={() => {
-          if (inputValue.trim() === '') {
-            return;
-          }
+        onPress={() => {
           onAddTask(inputValue.trim());
           setInputValue('');
         }}
-        className={`bg-secondary transition ${
-          inputValue.trim() !== ''
-            ? 'cursor-pointer fill-primary'
-            : 'cursor-auto fill-white'
-        }`}
+        isDisabled={isDisabled}
+        className="bg-secondary fill-white"
       >
         <Plus />
       </Button>
