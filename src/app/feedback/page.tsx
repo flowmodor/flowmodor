ec8ba2705dfd71dde8396a2c85c85c93c0992ce7
Max Shen <m4xshen@gmail.com>
2024-01-23 14:52:51 +0800

    feat: add feedback github link

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -6,6 +6,7 @@ import { Textarea } from '@nextui-org/input';
 import supabase from '@/utils/supabase';
 import Menu from '@/components/Menu';
 import { toast } from 'react-toastify';
+import { Link } from '@nextui-org/link';
 
 export default function Feedback() {
   const [value, setValue] = useState('');
@@ -32,9 +33,26 @@ export default function Feedback() {
   };
 
   return (
-    <div className="mt-10 w-screen px-10 sm:w-[70vw] md:w-[50vw] lg:w-[30vw]">
+    <div className="mt-10 w-screen px-10 text-center sm:w-[70vw] md:w-[50vw] lg:w-[40vw]">
       <Menu />
       <h1 className="text-3xl font-semibold">Feedback</h1>
+      <div className="mt-10 text-sm">
+        If you have GitHub account, please use{' '}
+        <Link
+          href="https://github.com/flowmodor/flowmodor/issues"
+          className="text-sm"
+        >
+          Issue tab
+        </Link>{' '}
+        for bug report, and{' '}
+        <Link
+          href="https://github.com/flowmodor/flowmodor/discussions/categories/feature-request"
+          className="text-sm"
+        >
+          Discussion tab
+        </Link>{' '}
+        for feature request.
+      </div>
       <Textarea
         value={value}
         onValueChange={setValue}
