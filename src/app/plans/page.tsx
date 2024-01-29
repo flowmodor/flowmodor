4a40b597bff5f5e62eac6717f369d9481e27036d
Max Shen <m4xshen@gmail.com>
2024-01-29 21:19:50 +0800

    refactor: refine plans page

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -7,57 +7,19 @@ import StarterButton from '@/components/Plans/StarterButton';
 import Menu from '@/components/Menu';
 import GoHome from '@/components/GoHome';
 import { getServerClient } from '@/utils/supabase';
-
-function Feature({ name }: { name: string }) {
-  return (
-    <li className="flex items-center gap-5">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-5 w-5 text-indigo-600"
-        viewBox="0 0 20 20"
-        fill="#DBBFFF"
-      >
-        <path
-          fillRule="evenodd"
-          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-          clipRule="evenodd"
-        />
-      </svg>
-      <span>{name}</span>
-    </li>
-  );
-}
-
-function Features({ names }: { names: string[] }) {
-  return (
-    <ul>
-      {names.map((name) => (
-        <Feature key={name} name={name} />
-      ))}
-    </ul>
-  );
-}
+import PlanCard from '@/components/Plans/PlanCard';
 
 export default async function Plans() {
-  const cookieStore = cookies();
-  const supabase = getServerClient(cookieStore);
+  const supabase = getServerClient(cookies());
+  const { data } = await supabase.from('plans').select('*').single();
 
-  const starter = ['Flowmodoro Timer', 'Task List'];
+  const starter = ['Flowmodoro Timer', 'Task List', 'Today Stats'];
   const pro = [
     'Everything in Starter',
     'Custom Break Ratio',
-    'Historical Statistics',
+    'Unlimited Stats',
     'Priority Support',
   ];
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  const { data } = await supabase
-    .from('plans')
-    .select('*')
-    .eq('user_id', user?.id)
-    .single();
 
   return (
     <>
@@ -82,25 +44,10 @@ export default async function Plans() {
           ) : null}
         </div>
         <div className="flex flex-col items-center justify-center gap-10 sm:flex-row sm:items-stretch">
-          <div className="flex w-full flex-col gap-10 rounded-xl bg-[#23223C] p-10 sm:w-2/5 lg:w-1/3">
-            <div className="flex flex-col gap-2">
-              <h2 className="text-2xl">Starter</h2>
-              <div className="flex items-end gap-1">
-                <h2 className="text-4xl font-semibold">$0</h2>
-                <div className="text-sm opacity-50">/ month</div>
-              </div>
-            </div>
+          <PlanCard name="Starter" price={0} features={starter}>
             <StarterButton data={data} />
-            <Features names={starter} />
-          </div>
-          <div className="flex w-full flex-col gap-10 rounded-xl bg-[#23223C] p-10 sm:w-2/5 lg:w-1/3">
-            <div className="flex flex-col gap-2">
-              <h2 className="text-2xl">Pro</h2>
-              <div className="flex items-end gap-1">
-                <h2 className="text-4xl font-semibold">$5</h2>
-                <div className="text-sm opacity-50">/ month</div>
-              </div>
-            </div>
+          </PlanCard>
+          <PlanCard name="Pro" price={5} features={pro}>
             <Button
               isDisabled={data?.status === 'ACTIVE'}
               as={Link}
@@ -111,8 +58,7 @@ export default async function Plans() {
             >
               {data?.status === 'ACTIVE' ? 'Current plan' : 'Upgrade to Pro'}
             </Button>
-            <Features names={pro} />
-          </div>
+          </PlanCard>
         </div>
       </div>
     </>
