093ea1ad3101a0538425763e0ea2a6125448b8a2
Max Shen <m4xshen@gmail.com>
2024-02-05 23:00:54 +0800

    style: apply prettier

--- src/components/TasksTab/TaskBox.tsx
+++ src/components/TasksTab/TaskBox.tsx
@@ -10,41 +10,46 @@ interface Props {
 
 export default function TaskBox({ task }: Props) {
   const { isRunning, mode } = useTimerStore((state) => state);
-  const { focusingTask, doCompleteTask, undoCompleteTask } = useTasksStore((state) => state);
-  const toastId = "task-toast"
+  const { focusingTask, doCompleteTask, undoCompleteTask } = useTasksStore(
+    (state) => state,
+  );
+  const toastId = 'task-toast';
 
   const taskToast = (text: string, options?: UpdateOptions<unknown>) => {
-    if (toast.isActive(toastId)){
-      toast.update(toastId, { ...options, render: text || options?.render, closeButton: options?.closeButton || undefined, toastId})
-    return toastId}
+    if (toast.isActive(toastId)) {
+      toast.update(toastId, {
+        ...options,
+        render: text || options?.render,
+        closeButton: options?.closeButton || undefined,
+        toastId,
+      });
+      return toastId;
+    }
 
-    let newOptions = {}
+    let newOptions = {};
 
     if (options) {
-      newOptions = {...options, className: options.className || undefined}
+      newOptions = { ...options, className: options.className || undefined };
     }
-    return toast(text, { ...newOptions, toastId})
-  }
+    return toast(text, { ...newOptions, toastId });
+  };
 
-  const undoCompleteToast = () => taskToast(`Task ${task.name} undone.`)
+  const undoCompleteToast = () => taskToast(`Task ${task.name} undone.`);
 
-  const undoComplete = async () => {undoCompleteToast();
+  const undoComplete = async () => {
+    undoCompleteToast();
     await undoCompleteTask(task);
-  }
+  };
 
-  const undoButton = () => (
-    <Button
-      onClick={() => undoComplete()} >
-      UNDO
-    </Button>
-  )
+  const undoButton = () => <Button onClick={() => undoComplete()}>UNDO</Button>;
 
-  const completeToast = () => taskToast(`Task ${task.name} completed.`, { closeButton: undoButton })
+  const completeToast = () =>
+    taskToast(`Task ${task.name} completed.`, { closeButton: undoButton });
 
   const doComplete = async () => {
     completeToast();
-    await doCompleteTask(task)
-  }
+    await doCompleteTask(task);
+  };
 
   return (
     <div className="flex min-h-[4rem] items-center border-b border-b-secondary px-4">
