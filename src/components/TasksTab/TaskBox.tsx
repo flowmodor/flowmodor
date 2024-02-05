6e6edbbc1340318f49637f71cb7123789c71d6b4
Max Shen <m4xshen@gmail.com>
2024-02-05 23:50:55 +0800

    feat: refine undo button style

--- src/components/TasksTab/TaskBox.tsx
+++ src/components/TasksTab/TaskBox.tsx
@@ -16,6 +16,8 @@ export default function TaskBox({ task }: Props) {
 
   const undoButton = () => (
     <Button
+      color="secondary"
+      radius="sm"
       onClick={async () => {
         await undoCompleteTask(task);
       }}
