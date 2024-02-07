df968e542aed138946dddb805050213dde053d46
Max Shen <m4xshen@gmail.com>
2024-02-07 13:56:11 +0800

    revert: add vercel analytics
    This reverts commit aba1111a8ed3701ed61656e21189c2895784efe5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://flowmodor.com',
-  integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  integrations: [react(), tailwind()]
 });
