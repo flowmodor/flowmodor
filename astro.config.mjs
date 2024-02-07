aba1111a8ed3701ed61656e21189c2895784efe5
Max Shen <m4xshen@gmail.com>
2024-02-07 13:53:40 +0800

    chore: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://flowmodor.com',
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
