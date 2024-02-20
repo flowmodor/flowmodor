947ca6eaf01675c086328a50d52afbc3204ae27d
Max Shen <m4xshen@gmail.com>
2024-02-21 07:45:05 +0800

    feat: refine thumbnail image

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,17 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: '-25px -25px 30px -15px rgba(0, 0, 0, 0.25)',
+      },
+    },
     fontFamily: {
-      sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+      sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
