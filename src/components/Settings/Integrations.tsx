301960dfc9aaec4c4d88c4dd56356f94c60b7cc8
Max Shen <m4xshen@gmail.com>
2024-02-25 16:47:42 +0800

    chore: disable todoist

--- src/components/Settings/Integrations.tsx
+++ src/components/Settings/Integrations.tsx
@@ -29,6 +29,7 @@ export default function Integrations({
         color="secondary"
         radius="sm"
         isLoading={isPending}
+        isDisabled
         onPress={() => {
           startTransition(async () => {
             if (provider === 'todoist') {
