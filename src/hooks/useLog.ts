622b73e341c3754c34d847180f8777c2c56f1332
Max Shen <m4xshen@gmail.com>
2024-02-28 09:09:15 +0800

    fix: log task id for task created on flowmodor

--- src/hooks/useLog.ts
+++ src/hooks/useLog.ts
@@ -1,47 +1,41 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import useTasksStore, { Task } from '@/stores/useTasksStore';
+import useTasksStore from '@/stores/useTasksStore';
 import useTimerStore from '@/stores/useTimerStore';
 import supabase from '@/utils/supabase';
-import getClient from '@/utils/todoist';
 
-async function log(
-  mode: string,
-  startTime: number,
-  endTime: number,
-  task: Task | null,
-) {
-  const start_time = new Date(startTime).toISOString();
-  const end_time = new Date(endTime).toISOString();
+export default function useLog() {
+  const { mode, startTime } = useTimerStore((state) => state);
+  const { focusingTask, activeList } = useTasksStore((state) => state);
+  const endTime = Date.now();
 
-  if (!task) {
+  async function log() {
+    const start_time = new Date(startTime!).toISOString();
+    const end_time = new Date(endTime).toISOString();
+
+    if (!focusingTask) {
+      await supabase.from('logs').insert([
+        {
+          mode,
+          start_time,
+          end_time,
+        },
+      ]);
+      return;
+    }
+
+    const hasId = activeList === 'Flowmodor - default';
     await supabase.from('logs').insert([
       {
         mode,
         start_time,
         end_time,
+        task_id: hasId ? focusingTask.id : null,
+        task_name: hasId ? null : focusingTask.name,
       },
     ]);
-    return;
   }
 
-  const todoist = await getClient();
-  await supabase.from('logs').insert([
-    {
-      mode,
-      start_time,
-      end_time,
-      task_id: todoist ? null : task.id,
-      task_name: todoist ? task.name : null,
-    },
-  ]);
-}
-
-export default function useLog() {
-  const { mode, startTime } = useTimerStore((state) => state);
-  const { focusingTask } = useTasksStore((state) => state);
-  const endTime = Date.now();
-
   return {
-    log: () => log(mode, startTime!, endTime, focusingTask),
+    log,
   };
 }
