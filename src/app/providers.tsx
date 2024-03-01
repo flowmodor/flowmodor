6c0c44bfc232733ff24baf142c1f9dfd116a3b86
Max Shen <m4xshen@gmail.com>
2024-03-01 19:48:40 +0800

    chore: setup nextui routing

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
 
 // eslint-disable-next-line import/prefer-default-export
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  const router = useRouter();
+  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
 }
