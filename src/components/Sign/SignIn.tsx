c379a951920106a21bd306415b3fc085df46f341
Max Shen <m4xshen@gmail.com>
2024-03-24 11:48:38 +0800

    refactor: refine oauth sign in animation

--- src/components/Sign/SignIn.tsx
+++ src/components/Sign/SignIn.tsx
@@ -32,7 +32,7 @@ export default function SignIn() {
           });
         }}
       >
-        <Google />
+        {isLoading ? null : <Google />}
         Continue with Google
       </Button>
       <Or />
