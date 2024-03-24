b3ccf11b2b6c9f611552dfdd4b132c423eed25aa
Max Shen <m4xshen@gmail.com>
2024-03-24 10:42:46 +0800

    refactor: remove redundant wrapper

--- src/app/(sign)/forgot-password/page.tsx
+++ src/app/(sign)/forgot-password/page.tsx
@@ -1,10 +1,47 @@
+import { Input } from '@nextui-org/input';
+import { Link } from '@nextui-org/link';
 import { Metadata } from 'next';
-import ForgotPassword from '@/components/Sign/ForgotPassword';
+import { sendPasswordReset } from '@/actions/auth';
+import Submit from '@/components/Submit';
 
 export const metadata: Metadata = {
   title: 'Forgot Password | Flowmodor',
 };
 
 export default function ForgotPasswordPage() {
-  return <ForgotPassword />;
+  return (
+    <form
+      action={sendPasswordReset}
+      className="flex flex-col gap-5 text-center sm:w-96"
+    >
+      <h1 className="text-3xl font-semibold">Forgot your password?</h1>
+      <div className="mx-auto w-[70vw] text-sm sm:w-auto">
+        To reset your password, please enter the email address of your Flowmodor
+        account.
+      </div>
+      <Input
+        name="email"
+        label="Email"
+        labelPlacement="outside"
+        placeholder="you@example.com"
+        variant="bordered"
+        type="email"
+        radius="sm"
+        classNames={{
+          base: 'my-5',
+          input: 'text-[16px]',
+          inputWrapper:
+            'border-secondary data-[hover=true]:border-secondary data-[focus=true]:!border-primary',
+        }}
+      />
+      <Submit>Reset my password</Submit>
+      <Link
+        href="/signin"
+        className="mx-auto text-sm text-white"
+        underline="always"
+      >
+        Go to signin
+      </Link>
+    </form>
+  );
 }
