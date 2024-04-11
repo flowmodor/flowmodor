f9a003e4010dd86407d3e3f008b0aae2a1fdaa11
Max Shen <m4xshen@gmail.com>
2024-04-11 20:45:53 +0800

    feat(stats): add period selector

--- src/components/Stats/DailyStats.tsx
+++ src/components/Stats/DailyStats.tsx
@@ -3,28 +3,62 @@
 import { Card, CardBody, CardFooter, CardHeader } from '@nextui-org/card';
 import { Chip } from '@nextui-org/chip';
 import { Link } from '@nextui-org/link';
+import { Select, SelectItem } from '@nextui-org/select';
 import { useRef } from 'react';
 import { Left, Right } from '@/components/Icons';
 import BarChart from '@/components/Stats/BarChart';
 import DateButton from '@/components/Stats/DateButton';
 import {
+  Period,
   useDisplayTime,
-  useEndDate,
+  useIsDisabled,
   useLogs,
+  usePeriod,
   useStatsActions,
 } from '@/stores/useStatsStore';
 
 export default function DailyStats({ isPro }: { isPro: boolean }) {
+  const periods: Period[] = ['day', 'week', 'month'];
   const logs = useLogs();
-  const { goPreviousTime, goNextTime } = useStatsActions();
-  const endDate = useEndDate();
+  const { goPreviousTime, goNextTime, onPeriodChange } = useStatsActions();
   const displayTime = useDisplayTime();
   const chartRef = useRef<any>(null);
+  const period = usePeriod();
+  const isDisabled = useIsDisabled();
 
   return (
     <Card radius="sm" className="bg-midground p-5">
       <CardHeader className="flex flex-col gap-1">
-        <div className="flex items-center gap-5 font-semibold">
+        <div className="flex justify-center w-full items-center gap-5 font-semibold">
+          <Select
+            size="sm"
+            radius="sm"
+            selectionMode="single"
+            label="Select a period"
+            classNames={{
+              trigger: 'bg-secondary data-[hover=true]:bg-secondary',
+              popoverContent: 'bg-background',
+              base: 'absolute left-5 top-5 max-w-[10rem]',
+            }}
+            selectedKeys={[period]}
+            onChange={(e) => {
+              const newPeriod = e.target.value;
+              if (newPeriod !== '') {
+                onPeriodChange(newPeriod as Period);
+              }
+            }}
+          >
+            {periods.map((p) => (
+              <SelectItem
+                key={p}
+                classNames={{
+                  base: 'data-[focus=true]:!bg-secondary data-[hover=true]:!bg-secondary',
+                }}
+              >
+                {p}
+              </SelectItem>
+            ))}
+          </Select>
           <DateButton
             onPress={goPreviousTime}
             ariaLabel="Previous day"
@@ -36,7 +70,7 @@ export default function DailyStats({ isPro }: { isPro: boolean }) {
           <DateButton
             onPress={goNextTime}
             ariaLabel="Next day"
-            isDisabled={new Date().getDate() === endDate.getDate()}
+            isDisabled={isDisabled}
           >
             <Right />
           </DateButton>
--- src/stores/useStatsStore.ts
+++ src/stores/useStatsStore.ts
@@ -2,10 +2,12 @@ import { create } from 'zustand';
 import { LogsWithTasks } from '@/utils/stats/calculateTaskTime';
 import supabase from '@/utils/supabase';
 
+export type Period = 'day' | 'week' | 'month';
+
 interface State {
   startDate: Date;
   endDate: Date;
-  period: 'day' | 'week' | 'month';
+  period: Period;
   displayTime: string;
   logs: LogsWithTasks[] | null;
 }
@@ -14,6 +16,7 @@ interface Action {
   updateLogs: () => Promise<void>;
   goNextTime: () => void;
   goPreviousTime: () => void;
+  onPeriodChange: (period: Period) => void;
 }
 
 interface Store extends State {
@@ -24,18 +27,20 @@ export const useStatsStore = create<Store>((set) => ({
   startDate: new Date(),
   endDate: new Date(),
   period: 'day',
-  displayTime: new Date().toDateString(),
+  displayTime: new Date().toLocaleDateString(undefined, {
+    month: 'short',
+    day: 'numeric',
+  }),
   logs: null,
   actions: {
     updateLogs: async () => {
       const { startDate: sd, endDate: ed, period } = useStatsStore.getState();
+      const startDate = new Date(sd);
+      startDate.setHours(0, 0, 0, 0);
+      const endDate = new Date(ed);
+      endDate.setHours(23, 59, 59, 999);
 
       if (period === 'day') {
-        const startDate = new Date(sd);
-        startDate.setHours(0, 0, 0, 0);
-        const endDate = new Date(ed);
-        endDate.setHours(23, 59, 59, 999);
-
         const { data, error } = await supabase
           .from('logs')
           .select('*, tasks(name)')
@@ -45,9 +50,22 @@ export const useStatsStore = create<Store>((set) => ({
         if (!error) {
           set({
             logs: data,
-            displayTime: startDate.toDateString(),
+            displayTime: startDate.toLocaleDateString(undefined, {
+              month: 'short',
+              day: 'numeric',
+            }),
           });
         }
+      } else if (period === 'week') {
+        set({
+          displayTime: `${startDate.toLocaleDateString(undefined, {
+            month: 'short',
+            day: 'numeric',
+          })} - ${endDate.toLocaleDateString(undefined, {
+            month: 'short',
+            day: 'numeric',
+          })}`,
+        });
       }
     },
     goNextTime: () => {
@@ -61,6 +79,18 @@ export const useStatsStore = create<Store>((set) => ({
           };
         }
 
+        if (state.period === 'week') {
+          const startDate = new Date(state.startDate);
+          startDate.setDate(startDate.getDate() + 7);
+          const endDate = new Date(startDate);
+          endDate.setDate(startDate.getDate() + 6);
+
+          return {
+            startDate,
+            endDate,
+          };
+        }
+
         return {};
       });
       useStatsStore.getState().actions.updateLogs();
@@ -70,9 +100,22 @@ export const useStatsStore = create<Store>((set) => ({
         if (state.period === 'day') {
           const yesterday = new Date(state.startDate);
           yesterday.setDate(state.startDate.getDate() - 1);
+
           return {
             startDate: yesterday,
             endDate: yesterday,
+          };
+        }
+
+        if (state.period === 'week') {
+          const startDate = new Date(state.startDate);
+          startDate.setDate(startDate.getDate() - 7);
+          const endDate = new Date(startDate);
+          endDate.setDate(startDate.getDate() + 6);
+
+          return {
+            startDate,
+            endDate,
           };
         }
 
@@ -80,13 +123,54 @@ export const useStatsStore = create<Store>((set) => ({
       });
       useStatsStore.getState().actions.updateLogs();
     },
+    onPeriodChange: (period: Period) => {
+      set(() => {
+        const startDate = new Date();
+        const endDate = new Date();
+
+        if (period === 'day') {
+          startDate.setHours(0, 0, 0, 0);
+          endDate.setHours(23, 59, 59, 999);
+        } else if (period === 'week') {
+          const day = startDate.getDay();
+          startDate.setDate(startDate.getDate() - day);
+          startDate.setHours(0, 0, 0, 0);
+          endDate.setDate(startDate.getDate() + 6);
+          endDate.setHours(23, 59, 59, 999);
+        }
+
+        return {
+          startDate,
+          endDate,
+          period,
+        };
+      });
+      useStatsStore.getState().actions.updateLogs();
+    },
   },
 }));
 
+export const usePeriod = () => useStatsStore((state) => state.period);
 export const useStartDate = () => useStatsStore((state) => state.startDate);
 export const useEndDate = () => useStatsStore((state) => state.endDate);
 export const useDisplayTime = () => useStatsStore((state) => state.displayTime);
 export const useLogs = () => useStatsStore((state) => state.logs);
 export const useStatsActions = () => useStatsStore((state) => state.actions);
+export const useIsDisabled = () => {
+  const period = usePeriod();
+  const startDate = useStartDate();
+  const endDate = useEndDate();
+  const today = new Date();
+
+  if (period === 'day') {
+    return today.getDate() === endDate.getDate();
+  }
+
+  if (period === 'week') {
+    return today >= startDate && today <= endDate;
+  }
+
+  return false;
+};
 
 useStatsStore.getState().actions.updateLogs();
