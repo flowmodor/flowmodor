05c6407f6e9236042615e6feb07732c94a75edf5
Max Shen <m4xshen@gmail.com>
2024-04-14 21:06:53 +0800

    feat: reset break ratio when pro expired

---
+++ supabase/migrations/20240414130613_reset_break_ratio.sql
@@ -0,0 +1,25 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.update_expired_integrations()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$begin
+    UPDATE public.integrations
+    SET access_token = null, provider = null
+    WHERE user_id IN (
+        SELECT user_id
+        FROM public.plans
+        WHERE end_time < CURRENT_TIMESTAMP
+    );
+
+    UPDATE public.settings
+    SET break_ratio = 5
+    WHERE user_id IN (
+        SELECT user_id
+        FROM public.plans
+        WHERE end_time < CURRENT_TIMESTAMP
+    );
+end$function$
+;
+
+
