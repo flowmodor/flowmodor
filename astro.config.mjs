5a62587e9bfce86893aa5550d502b37e7b70173d
Max Shen <m4xshen@gmail.com>
2024-05-25 19:54:15 +0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: 'https://flowmodor.com',
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()]
 });
