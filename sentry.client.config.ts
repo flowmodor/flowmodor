13e29649bd48df220796f5f53be8c7f19b7d9636
Max Shen <m4xshen@gmail.com>
2024-05-27 17:35:38 +0800

    chore: remove sentry

--- sentry.client.config.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-
-Sentry.init({
-  dsn: 'https://3c175d84b8134a76d916034795fff9c8@o4507322643316736.ingest.us.sentry.io/4507322645348352',
-  tracesSampleRate: 1,
-  debug: false,
-});
