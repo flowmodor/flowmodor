c5bdbf56a778ce5f289a3f7cd639afca959683bf
Max Shen <m4xshen@gmail.com>
2024-06-12 22:32:24 +0800

    feat: display time in tab title

--- src/components/TimerTab/Progress.tsx
+++ src/components/TimerTab/Progress.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+/* eslint-disable @next/next/no-head-element */
 import { CircularProgress } from '@nextui-org/progress';
 import {
   useDisplayTime,
   useMode,
   useShowTime,
+  useStatus,
   useTotalTime,
 } from '@/stores/useTimerStore';
 import { formatTime } from '@/utils';
@@ -13,34 +15,44 @@ export default function Progress() {
   const totalTime = useTotalTime();
   const displayTime = useDisplayTime();
   const mode = useMode();
+  const status = useStatus();
   const showTime = useShowTime();
 
   return (
-    <CircularProgress
-      id="progress"
-      value={
-        mode === 'focus'
-          ? 0
-          : 100 * (displayTime / Math.floor(totalTime! / 1000))
-      }
-      size="lg"
-      showValueLabel
-      aria-label="Timer progress"
-      valueLabel={
-        <div className="flex flex-col items-center gap-2">
-          {showTime ? formatTime(displayTime) : null}
-          <span className={showTime ? 'text-2xl' : 'text-3xl'}>
-            {mode === 'focus' ? 'Focus' : 'Break'}
-          </span>
-        </div>
-      }
-      classNames={{
-        svg: 'w-full sm:w-80 h-80 text-primary',
-        track: 'stroke-secondary',
-        value: `${
-          displayTime >= 3600 ? 'text-3xl sm:text-4xl' : 'text-4xl sm:text-5xl'
-        } font-semibold`,
-      }}
-    />
+    <>
+      {showTime && status !== 'idle' && (
+        <head>
+          <title>{formatTime(displayTime)}</title>
+        </head>
+      )}
+      <CircularProgress
+        id="progress"
+        value={
+          mode === 'focus'
+            ? 0
+            : 100 * (displayTime / Math.floor(totalTime! / 1000))
+        }
+        size="lg"
+        showValueLabel
+        aria-label="Timer progress"
+        valueLabel={
+          <div className="flex flex-col items-center gap-2">
+            {showTime ? formatTime(displayTime) : null}
+            <span className={showTime ? 'text-2xl' : 'text-3xl'}>
+              {mode === 'focus' ? 'Focus' : 'Break'}
+            </span>
+          </div>
+        }
+        classNames={{
+          svg: 'w-full sm:w-80 h-80 text-primary',
+          track: 'stroke-secondary',
+          value: `${
+            displayTime >= 3600
+              ? 'text-3xl sm:text-4xl'
+              : 'text-4xl sm:text-5xl'
+          } font-semibold`,
+        }}
+      />
+    </>
   );
 }
