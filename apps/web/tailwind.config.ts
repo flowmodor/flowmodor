5369802c7cf6aac0d8ff397214b48ba09a5049e3
Max Shen <m4xshen@gmail.com>
2024-08-11 13:21:08 +0800

    build: add pnpm workspace

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    '../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   plugins: [
