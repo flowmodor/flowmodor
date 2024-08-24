793cfcbacb6df19f0e8afcc961b41b80588ea019
Max Shen <m4xshen@gmail.com>
2024-08-24 13:42:22 +0800

    feat: basic auth flow

--- apps/mobile/src/app/(tabs)/_layout.tsx
+++ apps/mobile/src/app/(tabs)/_layout.tsx
@@ -1,36 +1,54 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Session } from '@supabase/supabase-js';
 import { Tabs } from 'expo-router';
-import React from 'react';
+import { ComponentProps, useEffect, useState } from 'react';
+import Auth from '@/src/components/Auth';
+import { supabase } from '@/src/utils/supabase';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
-  return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  if (session && session.user) {
+    return (
+      <Tabs
+        screenOptions={{
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Tab One',
+            tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="two"
+          options={{
+            title: 'Tab Two',
+            tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          }}
+        />
+      </Tabs>
+    );
+  }
+
+  return <Auth />;
 }
--- apps/mobile/src/app/(tabs)/index.tsx
+++ apps/mobile/src/app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { supabase } from '@/src/utils/supabase';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text>Tab One</Text>
+      <Button title="Sign Out" onPress={() => supabase.auth.signOut()} />
     </View>
   );
 }
