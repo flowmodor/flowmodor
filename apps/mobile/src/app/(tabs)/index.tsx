78808dd4ae91ffee982eedc7988fc964563bcf44
Max Shen <m4xshen@gmail.com>
2024-08-26 20:21:27 +0800

    feat: timer mock ui

--- apps/mobile/src/app/(tabs)/index.tsx
+++ apps/mobile/src/app/(tabs)/index.tsx
@@ -1,11 +1,34 @@
 import { StyleSheet, View } from 'react-native';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import { Pressable, Text } from '@/src/components/Themed';
 import { supabase } from '@/src/utils/supabase';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text>Tab One</Text>
+      <AnimatedCircularProgress
+        size={280}
+        width={32}
+        fill={10}
+        rotation={0}
+        tintColor="#DBBFFF"
+        backgroundColor="#3F3E55"
+        lineCap="round"
+      >
+        {() => (
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={styles.time}>00:00</Text>
+            <Text style={styles.mode}>Focus</Text>
+          </View>
+        )}
+      </AnimatedCircularProgress>
       <Pressable
         onPress={() => supabase.auth.signOut()}
         style={{
@@ -33,5 +56,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#131221',
     gap: 20,
+  },
+  time: {
+    fontSize: 44,
+    fontWeight: 'bold',
+  },
+  mode: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
