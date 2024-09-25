6a49178f426276ef755757468b5050bd79add73b
Max Shen <m4xshen@gmail.com>
2024-09-25 20:42:01 +0800

    fix(web): timer tab border

--- apps/web/src/components/TimerTab/index.tsx
+++ apps/web/src/components/TimerTab/index.tsx
@@ -3,7 +3,7 @@ import Toolbar from './Toolbar';
 
 export default function TimerTab() {
   return (
-    <div className="gap-3 rounded-lg lg:border-r lg:border-secondary h-[55vh] flex flex-col items-center justify-center  w-[90vw] bg-midground sm:h-[30rem] sm:w-[30rem]">
+    <div className="gap-3 rounded-lg lg:rounded-none lg:border-r lg:border-secondary h-[55vh] flex flex-col items-center justify-center  w-[90vw] bg-midground sm:h-[30rem] sm:w-[30rem]">
       <Progress />
       <Toolbar />
     </div>
