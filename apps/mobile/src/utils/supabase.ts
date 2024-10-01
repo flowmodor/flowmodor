e22d4949e26a793fa717437f4af888cb769499dc
Max Shen <m4xshen@gmail.com>
2024-10-01 15:07:55 +0800

    fix: add missing polyfill

--- apps/mobile/src/utils/supabase.ts
+++ apps/mobile/src/utils/supabase.ts
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
