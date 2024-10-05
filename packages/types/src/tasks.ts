dec028ad941dfd69fcbb6d33cd7bb431c4def4fe
Max Shen <m4xshen@gmail.com>
2024-10-05 10:54:37 +0800

    feat(types): export supabase type

--- packages/types/src/tasks.ts
+++ packages/types/src/tasks.ts
@@ -1,7 +1,10 @@
-import { Tables } from './supabase';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { Database, Tables } from './supabase';
 
 export type LogsWithTasks = Tables<'logs'> & {
   tasks: {
     name: string;
   } | null;
 };
+
+export type Supabase = SupabaseClient<Database>;
