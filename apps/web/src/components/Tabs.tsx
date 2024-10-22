36ea9e111475b96d74519767f61b3584bf41b893
Max Shen <m4xshen@gmail.com>
2024-10-23 07:42:09 +0800

    fix: tabs rwd

--- apps/web/src/components/Tabs.tsx
+++ apps/web/src/components/Tabs.tsx
@@ -13,7 +13,7 @@ export default function TabsWrapper({ children }: { children: ReactNode[] }) {
   }, []);
 
   return (
-    <div className="flex flex-col w-full gap-3">
+    <div className="flex h-full sm:h-auto p-3 flex-col w-full gap-3">
       <Tabs
         fullWidth
         classNames={{
--- apps/web/src/components/TimerTab/index.tsx
+++ apps/web/src/components/TimerTab/index.tsx
@@ -3,7 +3,7 @@ import Toolbar from './Toolbar';
 
 export default function TimerTab() {
   return (
-    <div className="gap-3 rounded-lg h-[55vh] flex flex-col items-center justify-center  w-[90vw] bg-midground sm:h-[30rem] sm:w-[30rem]">
+    <div className="gap-3 rounded-lg h-full py-2 flex flex-col items-center justify-center  w-[90vw] bg-midground sm:h-[30rem] sm:w-[30rem]">
       <Progress />
       <Toolbar />
     </div>
