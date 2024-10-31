4fa60e01c1da088710d9f8c2f8c892477d0fdd6c
Max Shen <m4xshen@gmail.com>
2024-10-31 14:40:38 +0800

    fix: update title with document.title

--- apps/web/src/components/TimerTab/Progress.tsx
+++ apps/web/src/components/TimerTab/Progress.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-/* eslint-disable @next/next/no-head-element */
 import { CircularProgress } from '@nextui-org/progress';
+import { useEffect } from 'react';
 import {
   useDisplayTime,
   useMode,
@@ -19,44 +19,40 @@ export default function Progress() {
   const status = useStatus();
   const showTime = useShowTime();
 
+  useEffect(() => {
+    document.title =
+      showTime && status !== 'idle' ? formatTime(displayTime) : 'Flowmodor';
+  }, [displayTime, showTime, status]);
+
   return (
-    <>
-      {showTime && status !== 'idle' && (
-        <head>
-          <title>{formatTime(displayTime)}</title>
-        </head>
-      )}
-      <CircularProgress
-        id="progress"
-        value={
-          mode === 'focus'
-            ? 0
-            : 100 * (displayTime / Math.floor(totalTime! / 1000))
-        }
-        size="lg"
-        showValueLabel
-        aria-label="Timer progress"
-        valueLabel={
-          <div className="flex flex-col items-center gap-2">
-            <span className={showTime ? 'text-2xl' : 'text-3xl'}>
-              {mode === 'focus' ? 'Focus' : 'Break'}
-            </span>
-            {showTime && formatTime(displayTime)}
-            <span className="text-sm font-normal">
-              <FocusingTask />
-            </span>
-          </div>
-        }
-        classNames={{
-          svg: 'w-full sm:w-80 h-80 text-primary',
-          track: 'stroke-secondary',
-          value: `${
-            displayTime >= 3600
-              ? 'text-3xl sm:text-4xl'
-              : 'text-4xl sm:text-5xl'
-          } font-semibold`,
-        }}
-      />
-    </>
+    <CircularProgress
+      id="progress"
+      value={
+        mode === 'focus'
+          ? 0
+          : 100 * (displayTime / Math.floor(totalTime! / 1000))
+      }
+      size="lg"
+      showValueLabel
+      aria-label="Timer progress"
+      valueLabel={
+        <div className="flex flex-col items-center gap-2">
+          <span className={showTime ? 'text-2xl' : 'text-3xl'}>
+            {mode === 'focus' ? 'Focus' : 'Break'}
+          </span>
+          {showTime && formatTime(displayTime)}
+          <span className="text-sm font-normal">
+            <FocusingTask />
+          </span>
+        </div>
+      }
+      classNames={{
+        svg: 'w-full sm:w-80 h-80 text-primary',
+        track: 'stroke-secondary',
+        value: `${
+          displayTime >= 3600 ? 'text-3xl sm:text-4xl' : 'text-4xl sm:text-5xl'
+        } font-semibold`,
+      }}
+    />
   );
 }
