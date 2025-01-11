0314d4d76bd506a0f09a0a984b3ed63ef5d0ceec
Max Shen <m4xshen@gmail.com>
2025-01-11 14:11:02 +0800

    feat(web): add logos to connect todo cta

--- apps/web/src/components/TasksTab/SourceSelector.tsx
+++ apps/web/src/components/TasksTab/SourceSelector.tsx
@@ -2,6 +2,7 @@
 
 import { Source } from '@flowmodor/task-sources';
 import { Select, SelectItem } from '@nextui-org/select';
+import Image from 'next/image';
 import Link from 'next/link';
 import {
   useActiveSource,
@@ -42,6 +43,25 @@ export default function SourceSelector() {
         }}
       >
         Connect your favorite todo list app
+        <Image alt="Todoist" width="18" height="18" src="/images/todoist.png" />
+        <Image
+          alt="TickTick"
+          width="18"
+          height="18"
+          src="/images/ticktick.png"
+        />
+        <Image
+          alt="Google Tasks"
+          width="18"
+          height="18"
+          src="/images/googletasks.png"
+        />
+        <Image
+          alt="Microsoft To Do"
+          width="18"
+          height="18"
+          src="/images/microsofttodo.png"
+        />
       </SelectItem>,
     );
   }
